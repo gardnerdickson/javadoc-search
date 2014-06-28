941bedff37baaad926db1d94787bb1ab51f2961e
Gardner Dickson <gardner.dickson@gmail.com>
2014-06-28 15:35:21 -0400

    Add functionality to get super and sub classes for a class

--- app/server/javadoc-search.py
+++ app/server/javadoc-search.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask import request
 from flask import jsonify
+import urllib.parse as urlparse
+
 from scraper import JavadocScraper
 
 app = Flask(__name__)
 
+_javadoc_scraper = JavadocScraper("http://docs.oracle.com/javase/7/docs/api")
 
 @app.route('/')
 def hello_world():
@@ -12,9 +16,18 @@ def hello_world():
 
 @app.route('/classes', methods=['GET'])
 def get_classes():
-    javadoc_scraper = JavadocScraper("http://docs.oracle.com/javase/7/docs/api")
-    classes = javadoc_scraper.retrieve_classes()
+    classes = _javadoc_scraper.retrieve_classes()
     return jsonify(classes)
 
+
+@app.route('/relatives', methods=['GET'])
+def get_hierarchy_classes():
+    encoded_url = request.args['url']
+    url = urlparse.unquote(encoded_url)
+
+    classes = _javadoc_scraper.retrieve_hierarchy_classes(url)
+    return jsonify(classes)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- app/server/scraper.py
+++ app/server/scraper.py
@@ -1,3 +1,5 @@
+from unicodedata import name
+
 __author__ = 'Gardner'
 
 import urllib.request as request
@@ -12,11 +14,10 @@ class JavadocScraper:
         self._packages_url = self._url_base + '/overview-frame.html'
 
     def retrieve_classes(self):
-        html_classes_response = request.urlopen(self._classes_url)
-        html_classes_doc = html5lib.parse(html_classes_response, encoding=html_classes_response.info().get_content_charset(), namespaceHTMLElements=False)
+        classes_doc = JavadocScraper._retrieve_response_as_doc(self._classes_url)
 
         classes = {}
-        class_links = html_classes_doc.findall('.//li/a')
+        class_links = classes_doc.findall('.//li/a')
         for class_link in class_links:
             class_type_and_package = class_link.attrib['title'].split(' in ')
             class_type = class_type_and_package[0]
@@ -38,3 +39,41 @@ class JavadocScraper:
             }
 
         return classes
+
+    def retrieve_hierarchy_classes(self, relative_class_url):
+        class_page_doc = self._retrieve_response_as_doc(self._url_base + '/' + relative_class_url)
+
+        super_classes = {}
+        sub_classes = {}
+
+        description_root = class_page_doc.find(".//div[@class='description']/ul[@class='blockList']/li[@class='blockList']")
+        for index, label in enumerate(description_root.findall('./dl/dt')):
+            if label.text == "All superinterfaces:":
+                super_classes = JavadocScraper._find_class_links(description_root, index)
+
+            elif label.text == "All Known Implementing Classes:":
+                sub_classes = JavadocScraper._find_class_links(description_root, index)
+
+        return {
+            'superClasses': super_classes,
+            'subClasses': sub_classes
+        }
+
+
+    @staticmethod
+    def _find_class_links(description_root, index):
+        classes = {}
+        class_links = description_root.findall('./dl[' + str(index + 1) + ']/dd/a')
+        for class_link in class_links:
+            class_name = class_link.text
+            url = class_link.attrib['href']
+            classes[class_name] = url
+
+        return classes
+
+
+    @staticmethod
+    def _retrieve_response_as_doc(url):
+        html_raw_response = request.urlopen(url)
+        html_doc = html5lib.parse(html_raw_response, encoding=html_raw_response.info().get_content_charset(), namespaceHTMLElements=False)
+        return html_doc
