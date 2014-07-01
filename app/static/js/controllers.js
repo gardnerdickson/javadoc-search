28ae03e760a15102da32643dcaf1f0be2a14ed9f
Gardner Dickson <gardner.dickson@gmail.com>
2014-07-01 14:46:22 -0400

    Retrieve javadoc packages from front end. Store search data in services

--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -1,18 +1,29 @@
 'use strict';
 
-app.controller('MainCtrl', ['$scope', '$log', 'javadocService', function($scope, $log, javadocService) {
+app.controller('MainCtrl', ['$scope', '$log', 'constants', 'javadocService', 'searchDataLocator', function($scope, $log, constants, javadocService, searchDataLocator) {
 
   $scope.javadocUrl = null;
-  $scope.classes = null;
+  $scope.display = null;
   $scope.loading = false;
 
   $scope.retrieveJavadocClasses = function() {
     $scope.loading = true;
     javadocService.retrieveClasses($scope.javadocUrl, function(classes) {
-      $scope.classes = angular.toJson(classes);
+      $scope.display = angular.toJson(classes);
       $scope.loading = false;
-    });
-  }
 
+      searchDataLocator.setSearchData(classes, constants.CLASSES);
+    });
+  };
+
+  $scope.retrieveJavadocPackages = function() {
+    $scope.loading = true;
+    javadocService.retrievePackages($scope.javadocUrl, function(packages) {
+      $scope.display = angular.toJson(packages);
+      $scope.loading = false;
+
+      searchDataLocator.setSearchData(packages, constants.PACKAGES);
+    });
+  };
 
 }]);
--- app/static/js/services.js
+++ app/static/js/services.js
@@ -6,15 +6,73 @@ app.service('javadocService', ['$http', function($http) {
 
   service.retrieveClasses = function(javadocUrl, onComplete) {
     var config = {
-      params: {
-        url: encodeURIComponent(javadocUrl)
-      }
+      params: { url: encodeURIComponent(javadocUrl) }
     };
     $http.get('./classes', config).then(function(response) {
       onComplete(response.data);
     });
   };
 
+  service.retrievePackages = function(javadocUrl, onComplete) {
+    var config = {
+      params: { url: encodeURIComponent(javadocUrl) }
+    };
+    $http.get('./packages', config).then(function(response) {
+      onComplete(response.data);
+    });
+  };
+
   return service;
+
+}]);
+
+
+app.service('constants', [function() {
+  var service = {};
+
+  service.metadata = {
+    CLASSES: 'Classes',
+    PACKAGES: 'Packages'
+  };
+
+  service.tryValidateMetadataType = function(type) {
+    if (type != service.metadata.CLASSES && type != service.metadata.PACKAGES) {
+      throw 'Invalid type: ' + type;
+    }
+  };
+
+  return service;
+}]);
+
+
+app.service('searchDataLocator', ['constants', function(constants) {
+
+  var service = {};
+
+  var searchData = {};
+
+  service.setSearchData = function(searchData, type) {
+    constants.tryValidateMetadataType(type);
+    searchData[type] = searchData;
+  };
+
+  service.getSearchData = function(type) {
+    constants.tryValidateMetadataType(type);
+    return searchData[type];
+  };
+
+  return service;
+}]);
+
+
+app.service('indexLocator', ['constants', function(constants) {
+
+  var service = {};
+
+  function Index(data) {
+    // TODO: subclasses for class search and package search.
+  }
+
+
 
 }]);
