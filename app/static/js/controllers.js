108fdfc9c473616b08864c140c052d9e363e23d5
Gardner Dickson <gardner.dickson@gmail.com>
2014-07-05 15:12:17 -0400

    Basic UI functionality

--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -1,20 +1,49 @@
 'use strict';
 
-app.controller('MainCtrl', ['$scope', '$log', 'constants', 'javadocService', 'searchDataLocator', 'indexLocator', function($scope, $log, constants, javadocService, searchDataLocator, indexLocator) {
+app.controller('MainCtrl', ['$scope', '$log', '$sce', 'constants', 'javadocService', 'searchDataLocator', 'indexLocator', function($scope, $log, $sce, constants, javadocService, searchDataLocator, indexLocator) {
 
+  var viewModes = {
+    ENTER_URL: 'EnterUrl',
+    LOADING_JAVADOC: 'LoadingJavadoc',
+    JAVADOC_LOADED: 'JavadocLoaded'
+  };
+
+  $scope.viewMode = viewModes.ENTER_URL;
   $scope.javadocUrl = null;
-  $scope.classRelativeUrl = null;
-  $scope.loading = false;
 
   $scope.retrieveJavadocClassesAndPackages = function() {
+
+    $scope.viewMode = viewModes.LOADING_JAVADOC;
+
+    loadClassesAndPackages(URI.encode($scope.javadocUrl) ,function() {
+      $scope.viewMode = viewModes.JAVADOC_LOADED;
+    });
+
+    loadJavadocSite($scope.javadocUrl);
+  };
+
+  $scope.retrieveJavadocClassRelatives = function() {
+    var url = new URI($scope.javadocUrl).segment($scope.classRelativeUrl);
+    var encodedUrl = URI.encode(url.toString());
+
+    javadocService.retrieveRelatives(encodedUrl, function(relatives) {
+      $scope.display = angular.toJson(relatives);
+    });
+  };
+
+  $scope.test = function() {
+    console.log("AppletContext < Applet", "AppletContext" < "Applet");
+    console.log("ArrayList < List", "ArrayList" < "List");
+    console.log("Collection < Thread", "Collection" < "Thread");
+  };
+
+
+  function loadClassesAndPackages(encodedUrl, onComplete) {
+
     var finished = {
       classes: false,
       packages: false
     };
-
-    $scope.loading = true;
-
-    var encodedUrl = URI.encode($scope.javadocUrl);
 
     javadocService.retrieveClasses(encodedUrl, function(classes) {
       searchDataLocator.setSearchData(classes, constants.metadata.CLASSES);
@@ -26,7 +55,7 @@ app.controller('MainCtrl', ['$scope', '$log', 'constants', 'javadocService', 'se
 
       finished.classes = true;
       if (!_.contains(_.values(finished), false)) {
-        $scope.loading = false;
+        onComplete();
       }
     });
 
@@ -36,28 +65,13 @@ app.controller('MainCtrl', ['$scope', '$log', 'constants', 'javadocService', 'se
 
       finished.packages = true;
       if (!_.contains(_.values(finished), false)) {
-        $scope.loading = false;
+        onComplete();
       }
     });
+  }
 
-  };
-
-  $scope.retrieveJavadocClassRelatives = function() {
-    $scope.loading = true;
-
-    var url = new URI($scope.javadocUrl).segment($scope.classRelativeUrl);
-    var encodedUrl = URI.encode(url.toString());
-
-    javadocService.retrieveRelatives(encodedUrl, function(relatives) {
-      $scope.display = angular.toJson(relatives);
-      $scope.loading = false;
-    });
-  };
-
-  $scope.test = function() {
-    console.log("AppletContext < Applet", "AppletContext" < "Applet");
-    console.log("ArrayList < List", "ArrayList" < "List");
-    console.log("Collection < Thread", "Collection" < "Thread");
-  };
+  function loadJavadocSite(url) {
+    $scope.iframeSource = $sce.trustAsResourceUrl(url)
+  }
 
 }]);
