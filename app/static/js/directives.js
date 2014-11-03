3265f3021cc3c663b30cd7fdf0569454ede7963e
Gardner Dickson <gardner.dickson@gmail.com>
2014-11-02 20:41:59 -0500

    Reworking how the results tree works.

--- app/static/js/directives.js
+++ app/static/js/directives.js
@@ -94,70 +94,167 @@ app.directive('searchResultMenu', ['$log', '$timeout', 'searchDataLocator', 'jav
     link: function(scope, element, attrs) {
 
       scope.SearchResultMenu = {};
-
-      var searchResultScopes = [];
-      scope.SearchResultMenu.addSearchResult = function(scope) {
-        searchResultScopes.push(scope);
-      };
-
-      scope.SearchResultMenu.removeSearchResult = function(scope) {
-        searchResultScopes.splice(searchResultScopes.indexOf(scope), 1);
-      };
+      scope.selectionTree = {}; // Not really a tree just a linear list that represent the selectable results currently visible.
 
       scope.SearchResultMenu.selectionMode = constants.selectionMode.CLASSES;
+      scope.SearchResultMenu.selectedClassIndex = -1;
+
 
       scope.SearchResultMenu.updateResults = function(searchResults) {
-        scope.SearchResultMenu.searchResults = searchResults;
-        console.log("Search result directives: ", searchResultScopes.length);
+        scope.SearchResultMenu.searchResults = [];
+        _.each(searchResults, function(result) {
+          scope.SearchResultMenu.searchResults.push({name: result});
+        });
       };
 
-      scope.SearchResultMenu.selectedClassIndex = -1;
+      scope.SearchResultMenu.setSearchResultScope = function(name, searchResultScope) {
+        _.each(scope.SearchResultMenu.searchResults, function(searchResult) {
+          if (searchResult.name === name) {
+            searchResult.scope = searchResultScope;
+          }
+        });
+      };
+
+//      scope.SearchResultMenu.addSearchResultChildScope = function(parentName, childName, childScope) {
+//        scope.selectionTree[parentName].children[childName] = childScope;
+//      };
+
+//      scope.SearchResultMenu.removeSearchResultScope = function(name) {
+//        delete scope.selectionTree[name];
+//      };
+
+
+//      scope.SearchResultMenu.insertResult = function(searchResults, parent) {
+//        $log.log("Inserting results into the search tree: ", searchResults);
+//        if (parent !== undefined) {
+//          var parentIndex = scope.selectionTree.indexOf(parent);
+//          if (parentIndex === -1) {
+//            throw "Could not find class in selection tree: " + parent;
+//          }
+//
+//          parentIndex++;
+//          _.each(searchResults, function(result) {
+//            scope.selectionTree.splice(parentIndex++, 0, {result: {}});
+//          });
+//        }
+//        else {
+//          _.each(searchResults, function(result) {
+//            scope.selectionTree.push({result: {}});
+//          });
+//        }
+//
+//        $log.log("selectionTree: ", scope.selectionTree);
+//      };
+
       scope.SearchResultMenu.selectedRelativeIndex = -1;
 
       keyPressWatcher.addHandler(keyPressWatcher.events.UP, function() {
         scope.$apply(function() {
-          if (scope.SearchResultMenu.selectionMode == constants.selectionMode.CLASSES) {
-            $log.log("decrementing selectedClassIndex");
-            scope.SearchResultMenu.selectedClassIndex--;
-            if (scope.SearchResultMenu.selectedClassIndex < 0) {
-              scope.SearchResultMenu.selectedClassIndex = 0;
+
+          if (scope.SearchResultMenu.searchResults.length === 0) {
+            return;
+          }
+
+          var foundSelected = false;
+          for (var i = 0; i < scope.SearchResultMenu.searchResults.length; i++) {
+            if (scope.SearchResultMenu.searchResults[i].scope.selected && i > 0) {
+              foundSelected = true;
+              if (i - 1 >= 0) {
+                scope.SearchResultMenu.searchResults[i].scope.selected = false;
+                scope.SearchResultMenu.searchResults[i - 1].scope.selected = true;
+              }
+              break;
             }
           }
-          else if (scope.SearchResultMenu.selectionMode == constants.selectionMode.RELATIVES) {
-            var selectedSearchResult = searchResultScopes[scope.SearchResultMenu.selectedClassIndex];
-            selectedSearchResult.selectedRelativeIndex--;
-            if (selectedSearchResult.selectedRelativeIndex < 0) {
-              selectedSearchResult.selectedRelativeIndex = 0;
-            }
+
+          if (!foundSelected) {
+            scope.SearchResultMenu.searchResults[0].scope.selected = true;
           }
+
+
+//          if (scope.SearchResultMenu.selectionMode == constants.selectionMode.CLASSES) {
+//
+//            if (_.contains(_.keys(scope.selectionTree), selectedClassName)) {
+//              scope.selectionTree[selectedClassName].scope.deselect();
+//            }
+//
+//            scope.SearchResultMenu.selectedClassIndex--;
+//            if (scope.SearchResultMenu.selectedClassIndex < 0) {
+//              scope.SearchResultMenu.selectedClassIndex = 0;
+//            }
+//
+//            selectedClassName = scope.SearchResultMenu.searchResults[scope.SearchResultMenu.selectedClassIndex];
+//            scope.selectionTree[selectedClassName].scope.select();
+//          }
+//          else if (scope.SearchResultMenu.selectionMode == constants.selectionMode.RELATIVES) {
+//            var selectedSearchResult = searchResultScopes[scope.SearchResultMenu.selectedClassIndex];
+//            selectedSearchResult.selectedRelativeIndex--;
+//            if (selectedSearchResult.selectedRelativeIndex < 0) {
+//              selectedSearchResult.selectedRelativeIndex = 0;
+//            }
+//          }
         });
       });
 
       keyPressWatcher.addHandler(keyPressWatcher.events.DOWN, function() {
         scope.$apply(function() {
-          if (scope.SearchResultMenu.selectionMode == constants.selectionMode.CLASSES) {
-            $log.log("incrementing selectedClassIndex");
-            scope.SearchResultMenu.selectedClassIndex++;
-            if (scope.SearchResultMenu.selectedClassIndex > scope.SearchResultMenu.searchResults.length - 1) {
-              scope.SearchResultMenu.selectedClassIndex = scope.SearchResultMenu.searchResults.length - 1;
+
+          if (scope.SearchResultMenu.searchResults.length === 0) {
+            return;
+          }
+
+          // find the selected class
+          var foundSelected = false;
+          for (var i = 0; i < scope.SearchResultMenu.searchResults.length; i++) {
+            if (scope.SearchResultMenu.searchResults[i].scope.selected) {
+              foundSelected = true;
+              if (i + 1 < scope.SearchResultMenu.searchResults.length) {
+                scope.SearchResultMenu.searchResults[i].scope.selected = false;
+                scope.SearchResultMenu.searchResults[i + 1].scope.selected = true;
+              }
+              break;
             }
           }
-          else if (scope.SearchResultMenu.selectionMode == constants.selectionMode.RELATIVES) {
-            var selectedSearchResult = searchResultScopes[scope.SearchResultMenu.selectedClassIndex];
-            selectedSearchResult.selectedRelativeIndex++;
-            if (selectedSearchResult.selectedRelativeIndex > selectedSearchResult.classRelatives.length - 1) {
-              selectedSearchResult.selectedRelativeIndex = selectedSearchResult.classRelatives.length - 1;
-            }
+
+          if (!foundSelected) {
+            scope.SearchResultMenu.searchResults[0].scope.selected = true;
           }
+
+//          if (scope.SearchResultMenu.selectionMode == constants.selectionMode.CLASSES) {
+//
+//            if (_.contains(_.keys(scope.selectionTree), selectedClassName)) {
+//              scope.selectionTree[selectedClassName].scope.deselect();
+//            }
+//
+//            scope.SearchResultMenu.selectedClassIndex++;
+//            if (scope.SearchResultMenu.selectedClassIndex > scope.SearchResultMenu.searchResults.length - 1) {
+//              scope.SearchResultMenu.selectedClassIndex = scope.SearchResultMenu.searchResults.length - 1;
+//            }
+//
+//            selectedClassName = scope.SearchResultMenu.searchResults[scope.SearchResultMenu.selectedClassIndex];
+//            scope.selectionTree[selectedClassName].scope.select();
+//
+//            $log.log("incrementing selectedClassIndex: ", scope.SearchResultMenu.selectedClassIndex);
+//          }
+//          else if (scope.SearchResultMenu.selectionMode == constants.selectionMode.RELATIVES) {
+//            var selectedSearchResult = searchResultScopes[scope.SearchResultMenu.selectedClassIndex];
+//            selectedSearchResult.selectedRelativeIndex++;
+//            if (selectedSearchResult.selectedRelativeIndex > selectedSearchResult.classRelatives.length - 1) {
+//              selectedSearchResult.selectedRelativeIndex = selectedSearchResult.classRelatives.length - 1;
+//            }
+//            $log.log("incrementing selectedClassIndex: ", scope.SearchResultMenu.selectedRelativeIndex);
+//          }
         });
       });
 
       keyPressWatcher.addHandler(keyPressWatcher.events.RIGHT, function() {
         scope.SearchResultMenu.selectionMode = constants.selectionMode.RELATIVES;
+        $log.log("switching selectionMode to ", scope.SearchResultMenu.selectionMode);
       });
 
       keyPressWatcher.addHandler(keyPressWatcher.events.LEFT, function() {
-        scope.SearchResultMenu.selectionMode = constants.selectionMode.CLASSES
+        scope.SearchResultMenu.selectionMode = constants.selectionMode.CLASSES;
+        $log.log("switching selectionMode to ", scope.SearchResultMenu.selectionMode);
       });
 
       keyPressWatcher.addHandler(keyPressWatcher.events.ENTER, function() {
@@ -182,12 +279,24 @@ app.directive('searchResult', ['$log', 'searchDataLocator', 'javadocService', 'k
 
       scope.SearchResult = {};
 
-      scope.classRelatives = [];
+      scope.classRelatives = {};
       scope.showRelatives = false;
+      scope.selected = false;
+      scope.name = scope.result.name;
 
       var className = attrs.className;
       var uniqueId = _.uniqueId();
       var relativesLoaded = false;
+
+
+      scope.setRelativeScope = function(name, scope) {
+        _.each(scope.classRelatives, function(relative) {
+          if (relative.name === name) {
+            relative.scope = scope;
+          }
+        });
+      };
+
 
       keyPressWatcher.addHandler(keyPressWatcher.events.LEFT, function() {
         var selectedClassName = scope.SearchResultMenu.searchResults[scope.SearchResultMenu.selectedClassIndex];
@@ -210,22 +319,40 @@ app.directive('searchResult', ['$log', 'searchDataLocator', 'javadocService', 'k
             var classInfo = searchDataLocator.getSearchData('Classes')[className];
 
             javadocService.retrieveRelatives(new URI(classInfo.url).toString(), function(relatives) {
-              scope.classRelatives = _.extend(_.keys(relatives.ancestors), _.keys(relatives.descendants));
+
+              var classRelatives = _.extend(_.keys(relatives.ancestors), _.keys(relatives.descendants));
               relativesLoaded = true;
+
+              _.each(classRelatives, function(relative) {
+                scope.classRelatives.push({name: relative});
+              });
+
+//              scope.SearchResultMenu.insertResult(scope.classRelatives.slice(), scope.name);
+
             });
           }
         }
       }, uniqueId);
 
 
-      scope.SearchResultMenu.addSearchResult(scope);
+//      scope.select = function() {
+//        if (!element.hasClass('selected')) {
+//          element.addClass('selected')
+//        }
+//      };
+//
+//      scope.deselect = function() {
+//        element.removeClass('selected');
+//      };
+
+
+      scope.SearchResultMenu.setSearchResultScope(scope.name, scope);
 
       element.on('$destroy', function() {
         keyPressWatcher.removeHandler(keyPressWatcher.events.UP, uniqueId);
         keyPressWatcher.removeHandler(keyPressWatcher.events.DOWN, uniqueId);
         keyPressWatcher.removeHandler(keyPressWatcher.events.LEFT, uniqueId);
         keyPressWatcher.removeHandler(keyPressWatcher.events.RIGHT, uniqueId);
-        scope.SearchResultMenu.removeSearchResult(scope);
       });
 
     }
@@ -239,9 +366,22 @@ app.directive('classRelative', [function() {
     restrict: 'A',
     link: function(scope, element, attrs) {
 
+      scope.selected = false;
+      scope.visible = true;
+
       scope.loadClassRelative = function(name) {
         scope.loadJavadocClassPage(name);
       };
+
+      scope.select = function() {
+        scope.selected = true;
+      };
+
+      scope.deselect = function() {
+        scope.selected = false;
+      };
+
+      scope.SearchResult.setRelativeScope(scope.$parent.name, name, scope);
 
     }
   }
